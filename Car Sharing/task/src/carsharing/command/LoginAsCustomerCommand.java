f1848270def6e2756837f838fa291e54043f6929
aleksandrsfridmans <alexnail@gmail.com>
2024-08-17 11:18:50 +0100

    Step 4. LoginAsCustomerCommand adjusted to return context dynamically

--- Car Sharing/task/src/carsharing/command/LoginAsCustomerCommand.java
+++ Car Sharing/task/src/carsharing/command/LoginAsCustomerCommand.java
@@ -1,10 +1,26 @@
 package carsharing.command;
 
+import carsharing.command.context.CustomerListMenu;
 import carsharing.command.context.MainMenu;
+import carsharing.dao.CustomerDao;
 
 public class LoginAsCustomerCommand implements Command {
+    private static final CustomerDao dao = CustomerDao.get();
+    private Context context;
+
+    @Override
+    public boolean execute() {
+        if (dao.getAll().isEmpty()) {
+            System.out.println("The customer list is empty!");
+            context = MainMenu.get();
+        } else {
+            context = CustomerListMenu.get();
+        }
+        return true;
+    }
+
     @Override
     public Context getContext() {
-        return MainMenu.get();
+        return context;
     }
 }
